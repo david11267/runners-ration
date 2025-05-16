75e2f76ff5c506d8357c2ebc3015f761de4d90b1
David Aslan <davidaslan112671@live.se>
2025-05-16 15:39:08 +0200

    added charming runner

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className="grid  grid-cols-6 ">
       <div className="col-start-2 col-span-4 ">
-        <h1 className="text-3xl text-center m-8"> Runners ration</h1>
+        <div className="flex justify-evenly">
+          <h1 className="text-3xl text-center m-8"> Runners ration</h1>
+          <img className="h-20" src="src\assets\runner.gif" />
+        </div>
         <RunForm />
       </div>
     </div>
